f5bc98938327bbe47e584e50c9413da9a0e67b16
Torbjørn B Ness <torbness@gmail.com>
2013-10-24 22:34:27 +0200

    New multitasking procedure

--- correlated_population/runscript_dend_1.0_2.sh
+++ correlated_population/runscript_dend_1.0_2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PBS -lnodes=8:ppn=16
+#PBS -lnodes=1:ppn=16
 #PBS -lwalltime=100:00:00
 #PBS -lpmem=2000MB
 #PBS -m abe
@@ -9,7 +9,7 @@
 folder=correlated_population
 filename=correlated_pop.py
 
-stimuli_pos=apic
+stimuli_pos=dend
 correlation=1.0
 
 cd /home/torbness/work/aLFP/$folder/
@@ -20,12 +20,14 @@ mkdir /global/work/torbness/aLFP/$folder/hay
 cp $filename /global/work/torbness/aLFP/$folder
 cd /global/work/torbness/aLFP/$folder/
 
-maxpartasks=128
-CELLS=10000
+maxpartasks=16
+#CELLS=10000
+START_CELL=3001
+END_CELL=5000
 
 python $filename make_population
 
-tasks=$(seq 0 $(($CELLS-1)))
+tasks=$(seq $START_CELL $END_CELL)
 for t in $tasks; do
         python $filename simulate_single_cell $correlation $stimuli_pos $t &	
         activetasks=$(jobs | wc -l)
